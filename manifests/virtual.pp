f07500a5a692d24c1fce4193392f64bfda4fe82f
Reid Vandewiele <marut@cat.pdx.edu>
2012-01-30 09:05:35 -0800

    Adding user dependency to file resources

--- manifests/virtual.pp
+++ manifests/virtual.pp
@@ -9,7 +9,7 @@ class amanda::virtual {
   }
 
   @user {
-    "${amanda::params::user}":
+    $amanda::params::user:
       uid     => $amanda::params::uid,
       gid     => $amanda::params::group,
       home    => $amanda::params::homedir,
@@ -22,6 +22,7 @@ class amanda::virtual {
   File {
     owner   => $amanda::params::user,
     group   => $amanda::params::group,
+    require => User[$amanda::params::user],
   }
 
   @file {
